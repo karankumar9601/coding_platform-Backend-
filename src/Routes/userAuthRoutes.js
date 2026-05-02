d98613a0daaf0988fea3fcd35b2900af9b80e372
karan kumar <karan@gmail.com>
2026-05-02 19:05:40 +0530

    implement delete user proflie

--- src/Routes/userAuthRoutes.js
+++ src/Routes/userAuthRoutes.js
@@ -1,12 +1,13 @@
 
 const express = require("express")
 const userRoute = express.Router();
-const { register, login, logout, getProfile } = require("../controller/usersAuth/userAuth")
+const { register, login, logout, getProfile,deleteProfile } = require("../controller/usersAuth/userAuth")
 const authMiddleware=require("../middleware/authMiddleware")
 
 userRoute.post('/register', register);
 userRoute.post('/login', login);
 userRoute.post('/logout',authMiddleware, logout);
 userRoute.get('/userProfile/:id',authMiddleware, getProfile);
+userRoute.delete('/deleteProfile',authMiddleware,deleteProfile)
 
 module.exports = userRoute;
