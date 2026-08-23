729d03060314e1f49f6a38a8396871a11f22d17f
karan kumar <karan@gmail.com>
2026-08-23 22:06:36 +0530

    reset configrutation of redis

--- src/config/redishConfig.js
+++ src/config/redishConfig.js
@@ -4,8 +4,8 @@ const redisClient = redis.createClient({
     username: 'default',
     password: process.env.REDIS_PASSWORD,
     socket: {
-        host: 'redis-13971.crce281.ap-south-1-3.ec2.cloud.redislabs.com',
-        port: 13971
+        host: 'redis-10325.c91.us-east-1-3.ec2.cloud.redislabs.com',
+        port: 10325
     }
 })
 
